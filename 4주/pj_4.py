3631d8d055b95c8a318aa01f5dfc04b2c26f06ab
rangx2 <163327846+rangx2@users.noreply.github.com>
2024-09-24 13:39:28 +0900

    과제

--- 4주/pj_4.py
+++ 4주/pj_4.py
@@ -5,9 +5,6 @@ img = cv2.imread("./cat2.jpg")
 if img is None:
     sys.exit("파일을 찾을 수 없습니다.")
     
-# 과제
-# I = round(0.299*R+0.587*G+0.114*B)
-    
 svt_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
     
 cv2.imshow("pj_4 Gray",svt_img)
